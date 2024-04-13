535c479e0d19001c55a1c5d08f1d490df4a85d7a
DynamicCatalyst <manpreet71221@gmail.com>
2024-04-13 18:45:09 +0530

    Updated Cesar Cypher and Organized Files
    improved Cesar cypher by fixing errors, and making it more user friendly.
    Organized programs and resources into separate folders.

---
+++ Cesar_Cypher/Cesar_Cypher.py
@@ -0,0 +1,28 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char ==" " or char ==type(int):
+      continue
+    position = alphabet.index(char)
+    new_position = position + shift_amount
+    end_text += alphabet[new_position]
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+import art
+print(art.logo)
+
+while True:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))%26
+  
+  caesar(start_text=text, shift_amount=shift, 
+  cipher_direction=direction)
+  stop = input("do you want to stop? Enter Y to stop : ")
+  if stop.lower() =="y":
+    break
